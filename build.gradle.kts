a5eec49ca6c78aecdf3ad54776a5043f7eaf9bf1
oberstrike <markus.juergens@gmx.de>
2020-05-13 11:25:45 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("org.openjfx:javafx-plugin:0.0.8")
+    }
+}
+apply(plugin = "org.openjfx.javafxplugin")
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.0"
+
+    // We need the javafx plugin since java 11
+    id("org.openjfx.javafxplugin") version "0.0.8"
+
+}
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+
+    // Add the tornadofx dependency
+    compile("no.tornado:tornadofx:1.7.17")
+}
+repositories {
+    mavenCentral()
+}
+
+javafx {
+    // Declare the javafx modules we need to use
+    modules("javafx.controls")
+}
+
+application {
+
+    // Declare the main class for the application plugin
+    mainClassName = "MyApp"
+}
+
+// Set the jvmTarget to 1.8 to support inlining
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
+
